3283017d9eaf785fdfdf9fa3a24b46432e380bad
AlexMorvi <ariel.morales@epn.edu.ec>
2022-02-27 14:05:14 -0500

    Creación de estructuras (6)

--- Program.cs
+++ Program.cs
@@ -5,6 +5,47 @@ namespace SftAsadero
 {
     internal static class Program
     {
+        public struct cliente
+        {
+            public string nombre;
+            public string cedula;
+            public string telefono;
+            public string direccion;
+        }
+        public struct fecha
+        {
+            public int dia;
+            public int mes;
+            public int anio;
+        }
+        public struct menuCombos
+        {
+            public int numeroCombo;
+            public double precioCombos;
+            public string contenidoCombo;
+        }
+        public struct menuPlatos
+        {
+            public int numeroPlatos;
+            public double precioPlatos;
+            public string contenidoPlatos;
+        }
+        public struct menuExtras
+        {
+            public int numeroExtras;
+            public double precioExtras;
+            public string contenidoExtras;
+        }
+        public struct factura
+        {
+            public cliente cliente;
+            public fecha fecha;
+            public menuCombos menuCombos;
+            public menuPlatos menuPlatos;
+            public menuExtras menuExtras;
+            public double subtotal;
+            public double total;
+        }
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
